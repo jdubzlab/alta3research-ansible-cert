989b93606c730994af57a82c6034d1492c100f44
jdubzlab <jdubzlab@gmail.com>
2023-11-14 03:50:32 +0000

    Second commit

---
+++ scripts/githubinfo.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+git ls-remote --get-url origin
+git remote show origin
---
+++ scripts/welcome.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl
+
+# Modules used
+use strict;
+use warnings;
+
+# Print functions
+print("Welcome to Jonathan Wilson's Ansible 101 Certification Program\n");
+
